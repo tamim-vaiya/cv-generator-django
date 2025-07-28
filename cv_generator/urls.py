aadde2dddf85ee3bacde05fe1a02108ecd9f0f44
Tamim <tamimvaiya.123@gmail.com>
2025-07-28 15:11:32 +0600

    Built accept view for form

--- cv_generator/urls.py
+++ cv_generator/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pdf.views import accept
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', accept, name='accept'),
 ]
