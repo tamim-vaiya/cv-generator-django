14664f14209ecbb671e796fbb7975c34b3f61db2
Tamim <tamimvaiya.123@gmail.com>
2025-07-28 15:22:16 +0600

    POST Method, sent data to database

--- pdf/views.py
+++ pdf/views.py
@@ -3,4 +3,30 @@ from .models import Profile
 # Create your views here.
 
 def accept(request):
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        summary = request.POST.get("summary")
+        degree = request.POST.get("degree")
+        school = request.POST.get("school")
+        university = request.POST.get("university")
+        previous_work = request.POST.get("previous_work")
+        skills = request.POST.get("skills")
+
+        profile = Profile(
+            name=name,
+            email=email,
+            phone=phone,
+            summary=summary,
+            degree=degree,
+            school=school,
+            university=university,
+            previous_work=previous_work,
+            skills=skills
+        )
+        profile.save()
+
+
     return render(request, 'pdf/accept.html')
